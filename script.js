5fb40ea3e5020464f271a5fe2624216c26b810d7
jinofuente <jinofuente.h@gmail.com>
2023-11-13 22:55:28 -0500

    terminado el javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+function agregarTarea(){
+    const tareaInput = document.getElementById('tarea-input');
+    const tareaText = tareaInput.ariaValueMax.trim();
+
+    if(tareaText === ""){
+        alert("Por favor, Ingrese una tarea");
+    }else{
+        const tareaList = document.getElementById('tarea-list');
+        const nuevaTarea = document.createElement("li");
+        
+        nuevaTarea.innerHTML = tareaText + " <button onclick = 'delete(this)'>Eliminar</button>";
+        tareaList.appendChild(nuevaTarea);
+        tareaInput.value = ""; 
+    }
+}
+
+function eliminar(tarea){
+    const tareaList1 = document.getElementById('tarea-list');
+    tareaList1.removeChild(tarea.parentNode);
+}
